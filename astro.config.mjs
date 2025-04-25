fe0a650516003c37fd7b9aaa59e6e449c2940267
faryd <mirandafaryd@gmail.com>
2025-04-24 23:27:31 -0500

    🚀 [ADD] configuration files for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://st0ckhunters.github.io",
+  base: "landingPage",
+});
